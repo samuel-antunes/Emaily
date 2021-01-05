1f4aef290c64c6a6462cc2d4db81e4105ed5edeb
Samuel Antunes <antunessamuel0@gmail.com>
2021-01-05 16:10:48 -0300

    first commit to github

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('./services/passport.js');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye:'buddy'});
-});
+require('./routes/authRoutes.js')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
